8bdd4b6d428fb7934612cc02721ae43dc043be7c
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2022-12-15 21:49:29 +0900

    [level 2] Title: 스킬트리, Time: 0.14 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+func solution(_ skill:String, _ skill_trees:[String]) -> Int {
+    
+    var skillTree = skill_trees
+    
+    for index in 0..<skillTree.count {
+        let someSkill = skillTree[index]
+        let skillString = someSkill.filter { c in
+            skill.contains(c)
+        }.map{String($0)}.joined()
+        skillTree[index] = skillString
+    }
+    
+    var isPossibleArray : [Bool] = []
+    
+    let skill = skill.map{String($0)}
+    
+    for skills in skillTree {
+        var index : Int = 0
+        let skills = skills.map{String($0)}
+        var isPossible : Bool = true
+        
+        while index < min(skill.count, skills.count) {
+            if skill[index] != skills[index] {
+                isPossible = false
+                break
+            }
+            index += 1
+        }
+        isPossibleArray.append(isPossible)
+    }
+    
+    
+    return isPossibleArray.filter{$0 == true}.count
+}
