3a248b3e00810b68b1d533388e90398b64ac9eba
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2023-04-18 23:30:33 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.62 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
@@ -1,5 +1,7 @@
 // filter에서 arr.min()을 계산하니까 오버헤드로 인해 시간 초과가 발생해서, min은 상수로 분리
 func solution(_ arr:[Int]) -> [Int] {
     let min = arr.min()
-    return arr.count == 1 ? [-1] : arr.filter{$0 != min}
+    return arr.count == 1 
+    ? [-1] 
+    : arr.filter{$0 != min}
 }
