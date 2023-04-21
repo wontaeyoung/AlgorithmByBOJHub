dbe3fb21af5f29e33851849a05822e8341830052
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2023-04-21 18:51:44 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.18 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.swift
@@ -0,0 +1,13 @@
+func solution(_ x:Int, _ n:Int) -> [Int64] {    
+    var ans : [Int64] = []
+    if x == 0 {
+        for _ in 0..<n {
+            ans.append(Int64(0))
+        }
+    } else {
+        for i in stride(from : x, to : x * n + x, by : x) {
+        ans.append(Int64(i))
+        }
+    }
+    return ans
+}
