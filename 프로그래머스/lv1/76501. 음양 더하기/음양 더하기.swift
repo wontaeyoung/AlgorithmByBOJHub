06821aa9115b41834568b8f2e367c20998563609
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2023-05-20 23:33:11 +0900

    [level 1] Title: 음양 더하기, Time: 0.90 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.swift
@@ -0,0 +1,8 @@
+// 22-09-29
+import Foundation
+
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    return Array(0..<signs.count)
+    .map{signs[$0] ? absolutes[$0] : -absolutes[$0]}
+    .reduce(0,+)
+}
