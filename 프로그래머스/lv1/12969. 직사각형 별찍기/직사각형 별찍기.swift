0591f8336704daac6eb040be994c029175f0da6d
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2023-05-25 23:04:13 +0900

    [level 1] Title: 직사각형 별찍기, Time: 7.38 ms, Memory: 14.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+
+let (a, b) = (n[0], n[1])
+
+for _ in 1...b {
+    print(String(repeating : "*", count : a))
+}
