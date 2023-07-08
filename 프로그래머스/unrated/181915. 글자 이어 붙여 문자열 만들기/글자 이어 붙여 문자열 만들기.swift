cdfa7cce45c660c7f40d03711460ab7392ee6300
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2023-07-08 21:41:08 +0900

    [unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.13 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ my_string:String, _ index_list:[Int]) -> String {
+    let myString = my_string.map{String($0)}
+    return index_list.map { index in
+        myString[index]
+    }.joined()
+}
