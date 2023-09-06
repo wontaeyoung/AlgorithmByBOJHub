003abdf7e69d2aece2e3616900757072a621b79f
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2023-09-06 23:58:45 +0900

    [level 2] Title: 스킬트리, Time: 0.14 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.swift
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.swift
@@ -5,7 +5,7 @@ func solution(_ skill:String, _ skill_trees:[String]) -> Int {
     var skillTree = skill_trees
     
     for index in 0..<skillTree.count {
-        let someSkill = skillTree[index]
+        let someSkill: String = skillTree[index]
         
         let skillString = someSkill.filter { c in
             skill.contains(c)
@@ -14,14 +14,14 @@ func solution(_ skill:String, _ skill_trees:[String]) -> Int {
         skillTree[index] = skillString
     }
     
-    var isPossibleArray : [Bool] = []
+    var isPossibleArray: [Bool] = []
     
-    let skill = skill.map{String($0)}
+    let skill: [String] = skill.map{String($0)}
     
     for skills in skillTree {
-        var index : Int = 0
-        let skills = skills.map{String($0)}
-        var isPossible : Bool = true
+        var index: Int = 0
+        let skills: [String] = skills.map{String($0)}
+        var isPossible: Bool = true
         
         while index < min(skill.count, skills.count) {
             if skill[index] != skills[index] {
@@ -33,5 +33,7 @@ func solution(_ skill:String, _ skill_trees:[String]) -> Int {
         isPossibleArray.append(isPossible)
     }
     
-    return isPossibleArray.filter{$0 == true}.count
+    return isPossibleArray
+    .filter{$0 == true}
+    .count
 }
