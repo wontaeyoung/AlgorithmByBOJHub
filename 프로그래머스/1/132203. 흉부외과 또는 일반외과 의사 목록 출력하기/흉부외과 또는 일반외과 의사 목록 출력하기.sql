5ef6f6150c6825ed59d3d307032829d85065f30f
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2024-10-06 23:16:09 +0900

    [level 1] Title: 흉부외과 또는 일반외과 의사 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -0,0 +1,13 @@
+SELECT
+    dr_name,
+    dr_id,
+    mcdp_cd,
+    DATE_FORMAT(hire_ymd, '%Y-%m-%d') as hire_ymd
+FROM
+    doctor
+WHERE
+    mcdp_cd = "GS"
+    OR mcdp_cd = "CS"
+ORDER BY
+    hire_ymd DESC,
+    dr_name
