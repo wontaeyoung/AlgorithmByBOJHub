9e4cd0c3893ea910336603459d72d9d526163793
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2024-10-10 22:29:18 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -0,0 +1,13 @@
+SELECT
+    id,
+    email,
+    first_name,
+    last_name
+FROM
+    developer_infos
+WHERE
+    SKILL_1 = "Python"
+    OR SKILL_2 = "Python"
+    OR SKILL_3 = "Python"
+ORDER BY
+    id
