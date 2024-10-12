96519e82b21378f9f5726a37c469f55615f4ec3c
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2024-10-12 23:35:33 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -7,11 +7,11 @@ SELECT
     DATE_FORMAT(ugr.created_date, "%Y-%m-%d") AS created_date
 FROM
     used_goods_board ugb
-JOIN
+LEFT JOIN
     used_goods_reply ugr ON ugr.board_id = ugb.board_id
 WHERE 
-    YEAR(ugb.created_date) = 2022
-    AND MONTH(ugb.created_date) = 10
+    YEAR(created_date) = 2022
+    AND MONTH(created_date) = 10
 ORDER BY
     ugr.created_date,
     ugb.title
