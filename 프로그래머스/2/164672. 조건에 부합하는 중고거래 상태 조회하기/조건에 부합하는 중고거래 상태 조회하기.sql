46faff065de2f05a7c320fefc775c95e469230f7
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2024-10-21 23:17:29 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,12 @@
+SELECT board_id,
+    writer_id,
+    title,
+    price,
+    CASE WHEN `status` = "SALE" THEN "판매중"
+        WHEN `status` = "RESERVED" THEN "예약중"
+        WHEN `status` = "DONE" THEN "거래완료"
+        ELSE `status` END AS `status`
+FROM used_goods_board
+WHERE DATE_FORMAT(created_date, "%y-%m-%d") = "22-10-05"
+ORDER BY board_id DESC
+    
