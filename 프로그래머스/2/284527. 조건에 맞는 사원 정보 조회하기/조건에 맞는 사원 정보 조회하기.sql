de88d692bc617299eddcff862dfab889678fd156
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2024-10-23 23:32:41 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,15 @@
+WITH grade AS (
+    SELECT emp_no,
+        SUM(score) AS score
+    FROM hr_grade
+    GROUP BY emp_no
+)
+
+SELECT g.score,
+    he.emp_no,
+    he.emp_name,
+    he.position,
+    he.email
+FROM hr_employees he JOIN grade g ON he.emp_no = g.emp_no
+ORDER BY g.score DESC
+LIMIT 1
