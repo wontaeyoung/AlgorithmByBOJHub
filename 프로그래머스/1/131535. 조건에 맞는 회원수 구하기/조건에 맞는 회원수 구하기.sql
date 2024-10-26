3150dc87190a3cf1e48919074f4c35abb7613f5c
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2024-10-26 21:54:07 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -1,9 +1,7 @@
-SELECT
-    COUNT(
+SELECT COUNT(
         CASE WHEN YEAR(joined) = 2021
             AND age BETWEEN 20 AND 29
         THEN 1
         ELSE NULL END
-    ) AS USERS
-FROM
-    user_info
+) AS USERS
+FROM user_info
