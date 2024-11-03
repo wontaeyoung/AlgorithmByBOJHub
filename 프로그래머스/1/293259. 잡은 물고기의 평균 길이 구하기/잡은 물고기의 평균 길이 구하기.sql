682fef29a8d0d422be65841ebbe3a554628842d2
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2024-11-03 23:07:31 +0900

    [level 1] Title: 잡은 물고기의 평균 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
+++ 프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
@@ -1,4 +1,2 @@
-SELECT
-    ROUND(AVG(CASE WHEN length IS NULL THEN 10 ELSE length END), 2) AS average_length
-FROM
-    fish_info
+SELECT ROUND(AVG(CASE WHEN length IS NULL THEN 10 ELSE length END), 2) AS average_length
+FROM fish_info
