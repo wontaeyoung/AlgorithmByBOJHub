d87200428f00aa02447dc797e9f5ab810926c70c
Taeyoung Won <45925685+wontaeyoung@users.noreply.github.com>
2024-11-10 22:27:38 +0900

    [level 2] Title: 특정 물고기를 잡은 총 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
+++ 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
@@ -1,3 +1,4 @@
 SELECT SUM(CASE WHEN fn.fish_name REGEXP("BASS|SNAPPER") THEN 1 ELSE 0 END) AS fish_count
-FROM fish_info fi JOIN fish_name_info fn
+FROM fish_info fi 
+    JOIN fish_name_info fn 
     USING(fish_type)
